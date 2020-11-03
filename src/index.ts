5b1fa5e15da333734ee32b5a78f0b424e7801abf
Bart Enkelaar <benkelaar@gmail.com>
2020-11-03 23:19:01 +0100

    WIP for local showcase

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-function drawTree() {
-    const div = document.createElement('div');
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter";
+ 
+export class TreeBuilderElement extends HTMLElement {
+  constructor(){
+    super();
+  }
+
+  public connectedCallback(): void {
+    this.drawTree();
+  }
+  
+  private drawTree() {
     const tree = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     tree.setAttribute('height', '400');
     tree.setAttribute('width', '450');
-
+  
     tree.innerHTML = '<path d="M 100 350 q -2 -25 5 -50 q 7 -25 5 -50 q -2 -25 5 -50 q -2 25 5 50 q 7 25 5 50 q -2 25 5 50 Z" stroke="black" stroke-width="1" fill="black" />';
   
-    div.appendChild(tree);
-    return div;
-  }
-  
-  document.body.appendChild(drawTree());
+    this.appendChild(tree);
+  }  
+}
+
+customElements.define('tree-builder', TreeBuilderElement);
+
+document.getElementsByTagName('tree-builder').item(0)?.appendChild(new TreeBuilderElement())
